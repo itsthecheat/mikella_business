90d006e58f8acc2f516ab75173a4b92e57ec77de
Leslie Behum <llaurenb@gmail.com>
2017-05-24 16:48:52 -0400

    add more styling, change colors

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,6 @@
 console.log('\'Allo \'Allo!');
 
 $(function(){
-  'use strict';
 
 // Change navbar color on scroll
     $(window).scroll(function(){
@@ -19,7 +18,7 @@ $(function(){
     $('.page-scroll a').bind('click', function(e) {
       var $element = $(this);
       $('html, body').stop().animate({
-        scrollTop: ($($element.attr('href')).offset().top - 100)
+        scrollTop: ($($element.attr('href')).offset().top - 125)
         }, 1150, 'easeInOutBack');
         e.preventDefault();
     });
@@ -27,6 +26,44 @@ $(function(){
     $('.navbar-collapse ul li a').click(function(){
             $('.navbar-toggle:visible').click();
     });
+
+
+  var contact = function(){
+    $('input:radio[name=contact]').on('click', function(e) {
+      var value = $('input:radio[name=contact]:checked').val();
+        if (value === "Phone") {
+          $('#contact-method').html('<input type="phone" name="phone" /><div id="input-text" class="label-text">Phone</div>').show('600');
+        } else if (value === "Email") {
+          $('#contact-method').html('<input type="email" name="email" /><div id="input-text" class="label-text">Email</div>').show('600');
+        } else {
+          $(this).hide('600')
+        }
+      });
+}();
+
+// var clearField = function(){
+//   $('input').focusout(function(e) {
+//     var value = $('input').val();
+//     if (value.length !== 0){
+//       $('.label-text').css({
+//                 'color': 'black',
+//                 'text-transform': 'uppercase',
+//                 '-moz-transform': 'translateY(-55px)',
+//                 '-ms-transform': 'translateY(-55px)',
+//                 '-webkit-transform': 'translateY(-55px)',
+//                 'transform': 'translateY(-55px)'
+//               })
+//     } else if (value.length === 0) {
+//       $('.label-text').css({
+//         '-moz-transform': 'translateY(-22px)',
+//         '-ms-transform': 'translateY(-22px)',
+//         '-webkit-transform': 'translateY(-22px)',
+//         'transform': 'translateY(-22px)',
+//         'transition': 'all 0.3s'
+//       })
+//     }
+//     });
+// }()
 
 
 })

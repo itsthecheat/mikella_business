1378e03df3d6fb06ae195fabd004b327ace2a91f
Leslie Behum <llaurenb@gmail.com>
2018-12-06 11:32:16 -0500

    update copy, add page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,59 +1,59 @@
-console.log('\'Allo \'Allo!');
+$(function(){
 
-(function($){
-"use strict";
-// Change navbar color on scroll
-    $(window).scroll(function(){
-      var top = $(window).scrollTop();
-      var navbar = $('.navbar-default');
+ 'use strict';
+ // Change navbar color on scroll
+     $(window).scroll(function(){
+       var top = $(window).scrollTop();
+       var navbar = $('.navbar-default');
 
-        if (top > 50){
-          navbar.addClass('scrolled');
-        } else {
-          navbar.removeClass('scrolled')
-        }
-    });
+         if (top > 50){
+           navbar.addClass('scrolled');
+         } else {
+           navbar.removeClass('scrolled')
+         }
+     });
 
 
-// Page Scroll transition effect when clilcking menu item
-    $('.page-scroll a').bind('click', function(e) {
-      var $element = $(this);
-      $('html, body').stop().animate({
-        scrollTop: ($($element.attr('href')).offset(). top - 100)
-        }, 1150, 'easeInOutBack');
-        e.preventDefault();
-    });
+ // Page Scroll transition effect when clilcking menu item
+     $('.page-scroll a').bind('click', function(e) {
+       var $element = $(this);
+       $('html, body').stop().animate({
+         scrollTop: ($($element.attr('href')).offset(). top - 100)
+         }, 1150, 'easeInOutBack');
+         e.preventDefault();
+     });
 
-    $(".page-scroll").click(function(event) {
-    $(".navbar-collapse").collapse('hide');
-  });
+     $('.page-scroll').click(function(e) {
+     $('.navbar-collapse').collapse('hide');
+   });
 
-//keep label on top of field when value is entered
-    function clearField() {
-      $('input').on('blur', function(e) {
-      var inputValue = this.value;
-        if (inputValue) {
-          this.classList.add('value-exists');
-        } else {
-          this.classList.remove('value-exists');
-        }
-      });
-    };
-    clearField();
+ //keep label on top of field when value is entered
+     function clearField() {
+       $('input').on('blur', function(e) {
+       var inputValue = this.value;
+         if (inputValue) {
+           this.classList.add('value-exists');
+         } else {
+           this.classList.remove('value-exists');
+         }
+       });
+     }
+     clearField();
 
-//change label based on chosen contact method
-    $('input:radio[name=method]').on('click', function(e) {
-      var value = $('input:radio[name=method]:checked').val();
-        if (value === 'Phone') {
-          $('#contact-method').html('<input type="phone" name="contact" /><div class="label-text">Phone</div>').show('600');
+ //change label based on chosen contact method
+     $('input:radio[name=method]').on('click', function(e) {
+       var value = $('input:radio[name=method]:checked').val();
+         if (value === 'Phone') {
+           $('#contact-method').html('<input type="phone" name="contact" /><div class="label-text">Phone</div>').show('600');
 
-        } else if (value === 'Email') {
-          $('#contact-method').html('<input type="email" name="contact" /><div class="label-text">Email</div>').show('600');
-        }
-        clearField();
-      });
+         } else if (value === 'Email') {
+           $('#contact-method').html('<input type="email" name="contact" /><div class="label-text">Email</div>').show('600');
+         }
+         clearField();
+       });
 
-})(jQuery);
+});
+
 
 //form validation
 // function validate() {
